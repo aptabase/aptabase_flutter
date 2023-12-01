9eae6a3fad31f239e1213bf91177370701e2d85a
pierre <pierre@weebi.com>
2023-12-01 18:55:08 +0100

    booo2

--- test/event_persist_test.dart
+++ test/event_persist_test.dart
@@ -1,6 +1,5 @@
 import 'package:aptabase_flutter/persistence.dart';
 import 'package:aptabase_flutter/src/offline_logic/event_offline.dart';
-import 'package:aptabase_flutter/src/offline_logic/services/sembast.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_memory.dart';
@@ -9,25 +8,24 @@ import 'init_service.dart';
 
 void main() async {
   final dbSembast = await databaseFactoryMemory.openDatabase('test.db');
-  final db = Database(dbSembast);
   test('save and get', () async {
     final dbStore = intMapStoreFactory.store('aptabase');
-    await dbStore.add(db.db, EventOffline.dummy.toMap());
-    await dbStore.add(db.db, EventOffline.dummy2.toMap());
+    await dbStore.add(dbSembast, EventOffline.dummy.toMap());
+    await dbStore.add(dbSembast, EventOffline.dummy2.toMap());
     // Build our app and trigger a frame.
-    final recordSnapshot = await dbStore.find(db.db);
+    final recordSnapshot = await dbStore.find(dbSembast);
     final events =
         recordSnapshot.map((e) => EventOffline.fromMap(e.value)).toList();
     expect(events.length, 2);
     // final events = getAllPersistedEvents(prefs);
     expect(events.last, EventOffline.dummy2);
-    await dbStore.delete(db.db);
+    await dbStore.delete(dbSembast);
   });
 
   test('test prevent from saving events if already 100', () async {
     final oneThousandEvents = List<EventOffline>.generate(
         100, (index) => const EventOffline('eventName'));
-    final service = TestEventService(db);
+    final service = TestEventService(dbSembast);
     for (final e in oneThousandEvents) {
       await service.addEvent.request(e);
     }
