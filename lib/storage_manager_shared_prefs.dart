0a673046e4e3788f359b55104fd268cfa26359bd
Gabriel Rozendo <gabriel.rozendo@gmail.com>
2024-11-06 22:59:28 +0000

    changed SharedPreferencesAsync to SharedPreferences as it was causing some errors

--- lib/storage_manager_shared_prefs.dart
+++ lib/storage_manager_shared_prefs.dart
@@ -2,13 +2,16 @@ import "package:aptabase_flutter/storage_manager.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
 class StorageManagerSharedPrefs extends StorageManager {
-  final _asyncPrefs = SharedPreferencesAsync();
-  var _events = <String, String>{};
+  final _events = <String, String>{};
 
   @override
   Future<void> init() async {
-    final items = await _asyncPrefs.getAll();
-    _events = items.cast<String, String>();
+    final sharedPrefs = await SharedPreferences.getInstance();
+    final keys = sharedPrefs.getKeys();
+    for (final key in keys) {
+      final value = sharedPrefs.getString(key);
+      if (value != null) _events[key] = value;
+    }
 
     return super.init();
   }
@@ -16,13 +19,19 @@ class StorageManagerSharedPrefs extends StorageManager {
   @override
   Future<void> addEvent(String key, String event) async {
     _events[key] = event;
-    await _asyncPrefs.setString(key, event);
+
+    final sharedPrefs = await SharedPreferences.getInstance();
+    await sharedPrefs.setString(key, event);
   }
 
   @override
   Future<void> deleteEvents(Set<String> keys) async {
     _events.removeWhere((k, _) => keys.contains(k));
-    await _asyncPrefs.clear(allowList: keys);
+
+    final sharedPrefs = await SharedPreferences.getInstance();
+    for (final key in keys) {
+      await sharedPrefs.remove(key);
+    }
   }
 
   @override
